b48e8bbdee821fc823995c298da76504fcd43575
Robert Foster <robert.foster2@nhm.ac.uk>
2020-11-07 14:56:40 +0000

    update

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://<yourappname>.azurewebsites.net:$PORT/predict 
+     -X POST https://rob-udacity-webapp.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
