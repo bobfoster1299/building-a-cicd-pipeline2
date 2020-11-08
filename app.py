28909a2e35cb1f7eda7118f44c6099befefaf3a0
Robert Foster <robert.foster2@nhm.ac.uk>
2020-11-08 16:39:15 +0000

    update

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask.logging import create_logger
 import logging
 
 import pandas as pd
-import joblib
-# from sklearn.externals import joblib
+#import joblib
+from sklearn.externals import joblib
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
